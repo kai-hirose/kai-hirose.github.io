6365e454e0fc3c2a7e24d0cd7417f92bcc10eeef
Kai Hirose <60566825+kai-hirose@users.noreply.github.com>
2021-06-24 13:29:56 -0700

    Added more components. Added styles and routes on header component.

--- gitpage/src/app/app-routing.module.ts
+++ gitpage/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AcademicsComponent } from './academics/academics.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'academics', component: AcademicsComponent }
 ];
 
 @NgModule({
