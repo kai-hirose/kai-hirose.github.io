fad02173558d153948ef4c6eb820c77f5a05b796
Kai Hirose <kaihiroseedu@gmail.com>
2024-05-31 20:09:57 -0700

    File transfer

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+{/*import { useState } from 'react'*/ }
+import TopHeader from './components/TopHeader'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-24">
+        <TopHeader></TopHeader>
+        <div className="flex flex-row lg:flex-row ">
+
+          <div className="w-11/12
+           lg:max-h-screen border-2 border-white"></div>
+
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
